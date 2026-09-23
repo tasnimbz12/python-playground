3b99d4c94cbdfe588bec491d9a6dd120bc605a74
Tasnim <213857271+tasnimbz12@users.noreply.github.com>
2026-09-23 11:53:56 +0100

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,9 @@
+# for loop in python
+for x in range(6):
+   print(x)
+for i in range(2, 6): #from 2 to 5
+    print(i)
+for j in range(2, 30, 3): #from 2 to 29 with step 3
+    print(j)
+for k in range(3):
+    print("i can jump")
